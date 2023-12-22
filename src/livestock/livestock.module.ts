4fbd01707118dd9d64cec4f81f56009914b3f4b7
Achref LAABIDI <abidi.achref95@gmail.com>
2023-12-22 02:03:45 +0100

    added some comments

--- src/livestock/livestock.module.ts
+++ src/livestock/livestock.module.ts
@@ -6,6 +6,7 @@ import { LiveStock, liveStockModel } from './livestock.model';
 
 
 @Module({
+  //need to import the model here too by using the forFeature method
   imports: [MongooseModule.forFeature([{ name: LiveStock.name, schema: liveStockModel }])],
   controllers: [LivestockController],
   providers: [LivestockService],
